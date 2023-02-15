f7312f959586046e34a5cfcd4aa60fb9f0e2c723
4l1fe <4l1fe@users.noreply.github.com>
2023-02-15 16:55:36 +0700

    Added FileData type, command args obj, reply fabric

--- run.py
+++ run.py
@@ -1,15 +1,32 @@
 import logging
 from dataclasses import dataclass
 from argparse import ArgumentParser
+from datetime import datetime, timezone
+from typing import Union
 
 import docker
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, filters
+from telegram.helpers import escape_markdown
+from telegram.constants import ParseMode
 
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
+
+class FileData:
+
+    def __init__(self, mode='r', type=str):
+        self.mode = mode
+        self.type = type
+
+    def __call__(self, file_name):
+        with open(file_name, self.mode) as file:
+            data = file.read().strip()
+
+        return self.type(data)
+        
 
 @dataclass
 class LogArgs:
@@ -18,7 +35,30 @@ class LogArgs:
 
     def __post_init__(self):
         self.tail_number = int(self.tail_number)
-    
+
+
+@dataclass
+class ListArgs:
+    limit: int = 15
+    all: bool = False
+
+    def __post_init__(self):
+        if isinstance(self.all, bool):  # Default value, no input
+            return
+
+        str_to_bool = {'t': True,
+                       'f': False} 
+        self.all = str_to_bool[self.all.lower()]
+
+
+@dataclass
+class RestartArgs:
+    container_name: str
+    timeout: int = 10
+
+    def __post_init__(self):
+        self.timeout = int(self.timeout)
+
 
 class ArgsHandler(CommandHandler):
 
@@ -29,7 +69,7 @@ class ArgsHandler(CommandHandler):
     def _wrapp_callback(self, command, callback, args_class):
 
         async def wrapped_callback(update, context):
-            logging.info('Command args: %s, %s', command, context.args)
+            logging.info('Command %s', update.message.text)
 
             try:
                 cbk_args = args_class(*context.args)
@@ -39,48 +79,81 @@ class ArgsHandler(CommandHandler):
                 await context.bot.send_message(chat_id=update.effective_chat.id, text=error)
                 return
 
-            return await callback(update, context, cbk_args)
+            docker_client = docker.from_env()
+            return await callback(update, context, cbk_args, docker_client)
 
         return wrapped_callback
 
 
+def reply_fabric(message, text) -> str:       
+    now = datetime.now(timezone.utc).isoformat(sep=' ', timespec='seconds')
+    
+    header = '_command_ ' + '`' + message.text + '`'
+    body = '\n\n'
+    body += escape_markdown(text, version=2)
+    body += '\n'
+    footer = '_replied_ ' + '`' + now + '`'
+
+    reply = header + body + footer
+    return reply
+
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Succeett")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Started")
 
 
-async def list_containers(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    dcl = docker.from_env()
-    c_list = dcl.containers.list(all=True)
-    text = 'list\n\n'
+async def user_info(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    info = str(update.message.from_user)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=info)
+
+
+async def list_containers(update: Update, context: ContextTypes.DEFAULT_TYPE,
+                          args: ListArgs, docker_client):
+    c_list = docker_client.containers.list(all=args.all, limit=args.limit)
+
+    text = ''
     for c in c_list:
         text += f'{c.name} {c.status}\n'
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+
+    reply = reply_fabric(update.message, text)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=reply,
+                                   parse_mode=ParseMode.MARKDOWN_V2)
 
 
-async def get_container_logs(update: Update, context: ContextTypes.DEFAULT_TYPE, args: LogArgs):
-    dcl = docker.from_env()
-    c = dcl.containers.get(args.container_name)
-    logs = c.logs(tail=args.tail_number)
-    logs = logs.decode()
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=logs)
+async def get_container_logs(update: Update, context: ContextTypes.DEFAULT_TYPE,
+                             args: LogArgs, docker_client):
+    c = docker_client.containers.get(args.container_name)
+
+    logs = c.logs(tail=args.tail_number).decode()
+    
+    reply = reply_fabric(update.message, logs)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=reply,
+                                   parse_mode=ParseMode.MARKDOWN_V2)
 
 
-async def restart_container(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    pass
+async def restart_container(update: Update, context: ContextTypes.DEFAULT_TYPE,
+                            args: RestartArgs, docker_client):
+    c = docker_client.containers.get(args.container_name)
+    
+    c.restart(timeout=args.timeout)
+
+    reply = reply_fabric(update.message, 'restarted\n')
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=reply,
+                                   parse_mode=ParseMode.MARKDOWN_V2)
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
-    parser.add_argument('token', help='Absolute path to a text file with a bot token in.')
+    parser.add_argument('token', type=FileData(), help='Absolute path to a text file with a bot token in.')
+    parser.add_argument('userid', type=FileData(type=int), help='Absolute path to a text file with an allowed user id number.')
     args = parser.parse_args()
     
-    with open(args.token, 'r') as file:
-        token = file.read().strip()
-        
-    application = ApplicationBuilder().token(token).build()
-    
-    application.add_handler(CommandHandler('start', start))
-    application.add_handler(CommandHandler('list', list_containers))
-    application.add_handler(ArgsHandler('logs', get_container_logs, LogArgs))
-    
+    application = ApplicationBuilder().token(args.token).build()
+    # application.add_handler(CommandHandler('start', start))
+    application.add_handler(CommandHandler('info', user_info))
+    application.add_handlers([
+        ArgsHandler('list', list_containers, ListArgs, filters=filters.User(user_id=args.userid)),
+        ArgsHandler('logs', get_container_logs, LogArgs, filters=filters.User(user_id=args.userid)),
+        ArgsHandler('restart', restart_container, RestartArgs, filters=filters.User(user_id=args.userid))
+    ])
     application.run_polling()
